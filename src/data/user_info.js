b2d586bf7297f786efc2392847b24d88f7e5b871
Vicky Kumar <kumarvky472@gmail.com>
2024-12-08 19:24:36 +0530

    first commit

---
+++ src/data/user_info.js
@@ -0,0 +1,116 @@
+const info = {
+    // ============ MAIN DETAILS ============
+    main: {
+        name: "Vicky Kumar",
+        description: "I am a software engineer with expertise in full-stack web development. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards.",
+        role: "Full-Stack Developer",
+        photo: "../IMG_1118.jpg",
+        email: "kumarvky472@gmail.com",
+    },
+
+    // ============ SOCIAL LINKS ============
+    socials: {
+        twitter: "https://x.com/_Vicky472",
+		github: "https://github.com/thevickykumar",
+		linkedin: "https://www.linkedin.com/in/thevickykumar/",
+		// instagram: "https://instagram.com/dsbalico",
+		// facebook: "https://facebook.com/dsbalico",
+    },
+
+    // ============ PROJECTS ============
+    projects: [
+        {
+			title: "CryptoWave",
+			description: "A full-stack crypto trading platform using Spring Boot, React, and MySQL, featuring real-time market data via Gemini API and CoinGecko API. Implemented secure wallet functionality, payment gateways (Razorpay, Stripe), and user authentication with Spring Security.",
+            technologies: "Java,SpringBoot,React,MySql,Tailwind",
+			github: "https://github.com/thevickykumar/cryptoWave",
+            link: "https://pseudocode-to-c.netlify.app/"
+		},
+
+        {
+			title: "QuizApp",
+			description: "Designed and developed a Quiz Application using Spring Boot and JPA for the backend and HTML, CSS, JS for the frontend. Enabled secure user authentication, dynamic quiz management, and real-time result tracking.",
+			technologies: "Java, SpringBoot, MySql, Javascript",
+			github: "https://github.com/thevickykumar/Quiz_App",
+            link: "https://github.com/dsbalico/YouShare"
+		},
+
+        {
+			title: "TapToChat",
+			description: "A real-time chat application using React, Node.js, and Socket.io for seamless messaging and notifications. Implemented user authentication with JWT and integrated Zustand for global state management.",
+			technologies: "React,Nodejs,Express.js,MongoDB",
+			github: "https://github.com/thevickykumar/TapToChat",
+            link: "https://letterdigitshape-classification.netlify.app/"
+		},
+
+		{
+			title: "CityLights",
+			description: "CityLights is a real estate website where user buy or rent houses and owner can add their flat/house for sale or rent.",
+			technologies: "React, Tailwind, Firebase",
+			github: "https://github.com/thevickykumar/CityLights",
+            link: "https://city-lights.vercel.app/"
+		},
+
+		{
+			title: "Thread Clone",
+			description: "Thread Clone App is a text-based conversation platform developed by Meta Platforms, the same company behind Instagram. It allows users to share their thoughts, opinions, and creativity in a threaded discussion format, similar to Twitter.",
+			technologies: "Nextjs, Tailwind, TypeScript",
+			github: "https://github.com/thevickykumar/ThreadClone",
+            link: "https://thread-clone-5a2d.vercel.app/login?callbackUrl=https%3A%2F%2Fthread-clone-5a2d.vercel.app%2F"
+		}
+    ],
+
+    // ============ EDUCATION ============
+    education: [
+        {
+            school: "C.V.Raman Global University, Bhubaneswar",
+            degree: "Bachelor of Technology in Computer Science and Information Technology",
+            duration: "2020 - 2024",
+            image: "https://imgs.search.brave.com/VZrpB7b7rGz-vw1BH0_LaFf98ElUpnY8jyRxjDOdzOs/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvZW4vdGh1bWIv/Ni82YS9DLl9WLl9S/YW1hbl9HbG9iYWxf/VW5pdmVyc2l0eV9M/b2dvLnBuZy81MTJw/eC1DLl9WLl9SYW1h/bl9HbG9iYWxfVW5p/dmVyc2l0eV9Mb2dv/LnBuZw"
+        },
+        
+    ],
+
+    // ============ EXPERIENCE ============
+    experience: [
+        {
+            position: "Software Developer Intern",
+            company: "RoadExpress, Gurugram",
+            duration: "July 2024 - Sep 2024 (3 Months)",
+            image: "https://www.roadexpress.in/img/rxp_png2.png",
+            descriptions: [
+                "RoadExpress Logistics is a leading logistics company founded in 2018 with the goal of providing efficient and reliable transportation solutions to businesses across the globe.",
+                "Contributed to developing a Delivery Tracking MIS using Node.js, Express.js, and PostgreSQL, streamlining order processing for multiple branches.",
+                "Conducted thorough testing of the application, ensuring reliability and performance across all user roles.",
+            ]
+        }
+    ],
+
+    // ============ CERTIFICATES ============
+    certificates: [
+        {
+            title: "CCNA: Switching, Routing, and Wireless Essentials",
+            description: "Provided  & Authorized by Cisco",
+            icon: "https://images.credly.com/size/680x680/images/f4ccdba9-dd65-4349-baad-8f05df116443/CCNASRWE__1_.png",
+            link: "https://www.credly.com/badges/ac07ec1e-81cd-4602-842d-82b291d89461/public_url",
+        },
+        {
+            title: "AWS Academy Graduate - AWS Academy Cloud Foundations",
+            description: "Provided & Authorized by AWS",
+            icon: "https://images.credly.com/size/680x680/images/73e4a58b-a8ef-41a3-a7db-9183dd269882/image.png",
+            link: "https://www.credly.com/badges/4680156f-cbef-4a89-9d53-27ffc61f5915/public_url",
+        }
+
+    ],
+
+    // ============ CONTACT ============
+    contact: {
+        title: "Let's Get in Touch: Ways to Connect with Me",
+        description: "Thank you for your interest in getting in touch with me. I welcome your feedback, questions, and suggestions. If you have a specific question or comment, please feel free to email me directly at kumarvky472@gmail.com. I make an effort to respond to all messages within 24 hours, although it may take me longer during busy periods. Finally, if you prefer to connect on social media, you can connect with me using the social media links below.",
+    },
+
+    // ============ FOOTER ============
+    footer: "© 2024 Vicky Kumar. All Rights Reserved"
+}
+
+export default info
